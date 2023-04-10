c40fdbc8c1f37a79c244f9ce2e69f6cb7639e65f
Dhruv Desai <ddesai7@wisc.edu>
2023-04-10 13:18:50 -0500

    Added a testing jpg

--- next_js/pages/index.tsx
+++ next_js/pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>This is the test App</h1>
+      <Image
+        src="public/spaceBg.jpg"
+        alt="Image Not Found"
+        width={728}
+        height={410}
+      />
+    </div>
+  );
 }
