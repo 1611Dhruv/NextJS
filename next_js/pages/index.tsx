9558b0677d0623ad9bc9f04b2d338de9798264e0
Dhruv Desai <ddesai7@wisc.edu>
2023-04-10 13:23:46 -0500

    Added changes to testApp

--- next_js/pages/index.tsx
+++ next_js/pages/index.tsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
+import bg from "../public/spaceBg.jpg";
 
 export default function Home() {
   return (
     <div>
       <h1>This is the test App</h1>
-      <Image
-        src="public/spaceBg.jpg"
-        alt="Image Not Found"
-        width={728}
-        height={410}
-      />
+      <Image src={bg} alt="Image Not Found" width={728} height={410} />
     </div>
   );
 }
