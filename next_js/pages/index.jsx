f11ebdf2e976e36983175f0bf3f220876e4fe36c
Dhruv Desai <ddesai7@wisc.edu>
2023-04-12 01:22:09 -0500

    I give up, tutorial time bois

--- next_js/pages/index.jsx
+++ next_js/pages/index.jsx
@@ -1,17 +1,41 @@
-import loadPokemon from "./pokemon.jsx";
-export default function Home() {
-  const data = loadPokemon({ name: "ditto" });
-  const { sprites } = data;
+import Image from "next/image.js";
 
-  console.log(data);
-  console.log(sprites);
+export default function Home({ image, types }) {
   return (
     <>
       <div className="main">
-        <div className="pokeDisplay">
-          <img></img>
-        </div>
+        <Pokemon image={image} types={types} />
+        <Pokemon image={image} types={types} />
+        <Pokemon image={image} types={types} />
+        <Pokemon image={image} types={types} />
       </div>
     </>
   );
+}
+
+export function Pokemon({ image, types }) {
+  return (
+    <div className="pokeDisplay">
+      <Image
+        src={image}
+        alt="notLoaded"
+        width={100}
+        height={100}
+        className="image"
+      />
+      <div className="content">
+        {types.map((each) => (
+          <p>{each.type.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon/gardevoir");
+  const content = await data.json();
+  return {
+    props: { image: content.sprites.front_default, types: content.types },
+  };
 }
