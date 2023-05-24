6fe1d26cdc77c95224adc32c821cea081f916f17
Dhruv Desai <ddesai7@wisc.edu>
2023-05-25 00:17:58 +0530

    Changed to the app format

---
+++ next_js/components/search.jsx
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <div className="search-container">
+      <input type="text" id="search" placeholder="Enter A Pokemon Name"></input>
+      <button>
+        <image src = ></image>
+      </button>
+    </div>
+  );
+}
