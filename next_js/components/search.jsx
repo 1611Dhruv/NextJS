3e33da7a9fabf5b267aca20a8499159f9f7d1d15
Dhruv Desai <ddesai7@wisc.edu>
2023-05-25 00:50:20 +0530

    Made stuff

--- next_js/components/search.jsx
+++ next_js/components/search.jsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import searchPic from "../public/search.png";
+import styles from "../styles/search.module.css";
+
 export default function Search() {
   return (
-    <div className="search-container">
-      <input type="text" id="search" placeholder="Enter A Pokemon Name"></input>
-      <button>
-        <image src = ></image>
+    <div className={styles.searchContainer}>
+      <input type="text" id="search" placeholder="Enter A Pokemon Name" />
+      <button onClick={() => console.log("fine")}>
+        <Image width="20" height="20" src={searchPic} alt=""></Image>
       </button>
     </div>
   );
